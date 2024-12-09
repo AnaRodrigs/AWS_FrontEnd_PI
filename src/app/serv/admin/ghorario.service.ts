85061c9142e38307e811c8d64287083f1444518b
AnaRodriggs <mariaanarodrigues2005@gmail.com>
2024-12-09 14:00:55 -0300

    second commit

--- src/app/serv/admin/ghorario.service.ts
+++ src/app/serv/admin/ghorario.service.ts
@@ -6,12 +6,12 @@
     providedIn: 'root',
   })
   export class GhorarioService {
-    private apiUrl = 'https://projeto-integrador-1v4i.onrender.com/reservation/';
-    private teachersUrl = 'https://projeto-integrador-1v4i.onrender.com/teacher/';
-    private subjectsUrl = 'https://projeto-integrador-1v4i.onrender.com/subject/';
-    private timesUrl = 'https://projeto-integrador-1v4i.onrender.com/time/';
-    private roomsUrl = 'https://projeto-integrador-1v4i.onrender.com/rooms/';
-    private coursesUrl = 'https://projeto-integrador-1v4i.onrender.com/course/';
+    private apiUrl = 'https://awsatividadepi.duckdns.org/reservation/';
+    private teachersUrl = 'https://awsatividadepi.duckdns.org/teacher/';
+    private subjectsUrl = 'https://awsatividadepi.duckdns.org/subject/';
+    private timesUrl = 'https://awsatividadepi.duckdns.org/time/';
+    private roomsUrl = 'https://awsatividadepi.duckdns.org/rooms/';
+    private coursesUrl = 'https://awsatividadepi.duckdns.org/course/';
 
     constructor(private http: HttpClient) {}
 
